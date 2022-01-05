af5a1254a93027b735345f53dc1ab33dea0b34fb
Amaan <89463628+Ammo-Nation@users.noreply.github.com>
2022-01-05 17:29:01 +0530

    via CS50

---
+++ Lists.py
@@ -0,0 +1,11 @@
+#Define a lists of names
+names = ['Harry','Ron','Hermoine','Ginny']
+
+print(names)
+
+names.append("Draco")
+
+names.sort()
+
+print(names)
+
---
+++ Loops.py
@@ -0,0 +1,13 @@
+# for i in [0,1,2,3,4,5]
+#     print (i)
+
+# or
+
+# for i in range(6):
+#     print (i)
+
+# loop in chars
+
+names=['Harry','Ron','Hermoine']
+for name in names:
+    print(name)
